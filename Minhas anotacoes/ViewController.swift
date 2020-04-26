7168b07ef56e54de0a90c7bf7e28f9a982cd52b9
Victor Rodrigues Novais <victor96novais@gmail.com>
2020-04-26 03:47:58 -0300

    Initial Commit

---
+++ Minhas anotacoes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Minhas anotacoes
+//
+//  Created by Victor Rodrigues Novais on 26/04/20.
+//  Copyright © 2020 Victoriano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
